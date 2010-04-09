8cb98029834bd48d66819fda9c4077fc1f374706
Jonas Folleso <jonas@follesoe.no>
2010-04-10 01:32:25 +0200

    Added a provider for the AudioPlayerTagger.

---
+++ HyperComments.Tests/AudioPlayerTaggerProviderTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HyperComments.Tests
+{
+    [TestClass]
+    public class AudioPlayerTaggerProviderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Throws_exception_if_buffer_is_null()
+        {
+            provider.CreateTagger<AudioPlayerTag>(null);
+        }
+
+        [TestMethod]
+        public void Creates_the_AudioPlayerTagger()
+        {
+            var buffer = new Mock<ITextBuffer>().Object;
+            var tagger = provider.CreateTagger<AudioPlayerTag>(buffer);
+
+            Assert.IsNotNull(tagger);
+            Assert.IsInstanceOfType(tagger, typeof(AudioPlayerTagger));
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var aggregatorService = new Mock<IClassifierAggregatorService>();
+            aggregatorService.Setup(a => a.GetClassifier(It.IsAny<ITextBuffer>()))
+                             .Returns(new Mock<IClassifier>().Object);
+
+            AggregatorService = aggregatorService.Object;
+
+            provider = new AudioPlayerTaggerProvider();                                 
+
+            var batch = new CompositionBatch();
+            batch.AddPart(this);
+            batch.AddPart(provider);
+
+            var catalog = new AggregateCatalog();
+            var container = new CompositionContainer(catalog);
+            container.Compose(batch);
+        }
+
+        [Export]
+        public IClassifierAggregatorService AggregatorService;
+
+        private AudioPlayerTaggerProvider provider;
+    }
+}
---
+++ HyperComments/AudioPlayerTaggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text.Classification;
+
+using Microsoft.VisualStudio.Utilities;
+
+namespace HyperComments
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("code")]
+    [TagType(typeof(AudioPlayerTag))]
+    public class AudioPlayerTaggerProvider : ITaggerProvider
+    {
+        [Import]
+        internal IClassifierAggregatorService AggregatorService;
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            if(buffer == null) throw new ArgumentException("buffer");
+
+            return new AudioPlayerTagger(AggregatorService.GetClassifier(buffer)) as ITagger<T>;
+        }
+    }
+}
