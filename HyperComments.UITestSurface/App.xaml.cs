027f563d299baf1120eb730776fd97a04d368c50
Jonas Folleso <jonas@follesoe.no>
2010-04-10 02:43:18 +0200

    Added new project that will be used as a test surface for UI components.

---
+++ HyperComments.UITestSurface/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace HyperComments.UITestSurface
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
