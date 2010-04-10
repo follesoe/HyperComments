b3572a32af3c7aa8c3a2caa16af6607fba3b4ce2
Jonas Folleso <jonas@follesoe.no>
2010-04-10 03:29:15 +0200

    Added audio player control with UI and most of the interaction.

---
+++ HyperComments/Player/AudioPlayer.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HyperComments.Player
+{
+	public partial class AudioPlayer : UserControl
+	{
+	    private const string CollapsedState = "Collapsed";
+	    private const string ExpandedState = "Expanded";
+	    private const string PlayState = "Play";
+	    private const string PauseState = "Pause";
+
+		public AudioPlayer()
+		{
+			InitializeComponent();
+
+            _playPause.Click += OnPlayPauseClick;            
+
+		    VisualStateManager.GoToState(this, CollapsedState, false);
+		}
+
+        private void OnPlayPauseClick(object sender, RoutedEventArgs e)
+        {
+            if(VisualStateGroup.CurrentState.Name == CollapsedState)
+            {
+                VisualStateManager.GoToState(this, ExpandedState, true);
+                VisualStateManager.GoToState(_playPause, PauseState, true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, CollapsedState, true);
+                VisualStateManager.GoToState(_playPause, PlayState, true);
+            }
+        }
+	}
+}
