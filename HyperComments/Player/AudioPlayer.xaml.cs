86f2ab14b1243d8f9c933d7ee900f5f915aa57af
Jonas Folleso <jonas@follesoe.no>
2010-04-10 03:41:50 +0200

    Added more interactivity to the position slider for the audio player.

--- HyperComments/Player/AudioPlayer.xaml.cs
+++ HyperComments/Player/AudioPlayer.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HyperComments.Player
 {
 	public partial class AudioPlayer : UserControl
 	{
+	    private const string HiddenState = "Hidden";
+	    private const string VisibleState = "Visible";
 	    private const string CollapsedState = "Collapsed";
 	    private const string ExpandedState = "Expanded";
 	    private const string PlayState = "Play";
@@ -14,8 +17,12 @@ namespace HyperComments.Player
 		{
 			InitializeComponent();
 
-            _playPause.Click += OnPlayPauseClick;            
+            _playPause.Click += OnPlayPauseClick;
 
+            MouseEnter += OnMouseEnter;
+            MouseLeave += OnMouseLeave;
+
+		    VisualStateManager.GoToState(this, HiddenState, false);
 		    VisualStateManager.GoToState(this, CollapsedState, false);
 		}
 
@@ -29,7 +36,24 @@ namespace HyperComments.Player
             else
             {
                 VisualStateManager.GoToState(this, CollapsedState, true);
+                VisualStateManager.GoToState(this, HiddenState, true);
                 VisualStateManager.GoToState(_playPause, PlayState, true);
+            }
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (VisualStateGroup.CurrentState.Name == ExpandedState)
+            {
+                VisualStateManager.GoToState(this, VisibleState, true);
+            }
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (VisualStateGroup.CurrentState.Name == ExpandedState)
+            {
+                VisualStateManager.GoToState(this, HiddenState, true);
             }
         }
 	}
