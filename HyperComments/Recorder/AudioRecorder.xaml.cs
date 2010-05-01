17fb2f53c24e58df972b40be91f1d49b9828fd94
Jonas Folles <jonas@follesoe.no>
2010-05-01 17:53:20 +0200

    Added animations to switch between recording/non-recording state for the recording control.

--- HyperComments/Recorder/AudioRecorder.xaml.cs
+++ HyperComments/Recorder/AudioRecorder.xaml.cs
@@ -1,7 +1,14 @@
-﻿namespace HyperComments.Recorder
+﻿using System.Windows;
+
+namespace HyperComments.Recorder
 {
     public partial class AudioRecorder
-    {        
+    {
+        private const string CollapsedState = "Collapsed";
+        private const string ExpandedState = "Expanded";
+        private const string RecordState = "Record";
+        private const string StopState = "Stop";
+
         public AudioRecorderViewModel ViewModel
         {
             get { return (AudioRecorderViewModel) DataContext; }
@@ -10,6 +17,24 @@
         public AudioRecorder()
         {
             InitializeComponent();
+
+            _record.Click += OnRecorderClick;
+
+            VisualStateManager.GoToState(this, CollapsedState, false);
+        }
+
+        public void OnRecorderClick(object sender, RoutedEventArgs e)
+        {
+            if(VisualStateGroup.CurrentState.Name == CollapsedState)
+            {
+                VisualStateManager.GoToState(this, ExpandedState, true);
+                VisualStateManager.GoToState(_record, StopState, true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, CollapsedState, true);
+                VisualStateManager.GoToState(_record, RecordState, true);
+            }
         }
     }
 }
