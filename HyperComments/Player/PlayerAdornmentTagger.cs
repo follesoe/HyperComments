82f5a6269dfd5cb49d1eaf0cffb2a37580976ce1
Jonas Folles <jonas@follesoe.no>
2010-05-26 20:43:10 +0200

    Added player adornment tagger.

---
+++ HyperComments/Player/PlayerAdornmentTagger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace HyperComments.Player
+{
+    public class PlayerAdornmentTagger : IntraTextAdornmentTagTransformer<PlayerTag, AudioPlayer>
+    {
+        public PlayerAdornmentTagger(IWpfTextView view, ITagAggregator<PlayerTag> dataTagger) : base(view, dataTagger)
+        {
+        }
+
+        public static ITagger<IntraTextAdornmentTag> GetTagger(IWpfTextView view, Lazy<ITagAggregator<PlayerTag>> playerTagger)
+        {
+            return view.Properties.GetOrCreateSingletonProperty(
+                () => new PlayerAdornmentTagger(view, playerTagger.Value));
+        }
+
+        protected override AudioPlayer CreateAdornment(PlayerTag data, SnapshotSpan span)
+        {
+            var player = new AudioPlayer();
+            player.ViewModel.Filename = data.Filename;
+            return player;            
+        }
+
+        protected override bool UpdateAdornment(AudioPlayer adornment, PlayerTag data)
+        {
+            adornment.ViewModel.Filename = data.Filename;
+            return true;
+        }
+
+        public override void Dispose()
+        {
+            view.Properties.RemoveProperty(typeof(PlayerAdornmentTagger));
+        }
+    }
+}
