d19cb3b8e0cad7e131ace5a73bc95ab5b2aff481
Jonas Folles <jonas@follesoe.no>
2010-05-26 20:56:32 +0200

    Added code to replace the recording comment with a player comment when recording is completed.

--- HyperComments/Recorder/RecorderAdornmentTagger.cs
+++ HyperComments/Recorder/RecorderAdornmentTagger.cs
@@ -7,8 +7,11 @@ namespace HyperComments.Recorder
 {
     public class RecorderAdornmentTagger : IntraTextAdornmentTagTransformer<RecorderTag, AudioRecorder>
     {
+        private readonly IWpfTextView _view;
+
         public RecorderAdornmentTagger(IWpfTextView view, ITagAggregator<RecorderTag> dataTagger) : base(view, dataTagger)
         {
+            _view = view;
         }
 
         public static ITagger<IntraTextAdornmentTag> GetTagger(IWpfTextView view, Lazy<ITagAggregator<RecorderTag>> recorderTagger)
@@ -20,13 +23,16 @@ namespace HyperComments.Recorder
         protected override AudioRecorder CreateAdornment(RecorderTag data, SnapshotSpan span)
         {
             var recorder = new AudioRecorder();
+            recorder.ViewModel.RecordingComplete += (o, e) => _view.TextBuffer.Replace(span, "// {audio: " + e.Filename + "}");
             recorder.ViewModel.ActiveDocument = data.ActiveDocument;
             recorder.ViewModel.RecordingDirectory = data.RecordingDirectory;
             return recorder;
-        }
+        }        
 
         protected override bool UpdateAdornment(AudioRecorder adornment, RecorderTag data)
         {
+            adornment.ViewModel.ActiveDocument = data.ActiveDocument;
+            adornment.ViewModel.RecordingDirectory = data.RecordingDirectory;
             return true;
         }
 
