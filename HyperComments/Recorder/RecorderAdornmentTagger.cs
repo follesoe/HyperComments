b6addef1abc036aad7f582f7e9ec3eebf7544a97
Jonas Folles <jonas@follesoe.no>
2010-05-26 20:02:33 +0200

    Added Recorder Adonrment Tagger.

---
+++ HyperComments/Recorder/RecorderAdornmentTagger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace HyperComments.Recorder
+{
+    public class RecorderAdornmentTagger : IntraTextAdornmentTagTransformer<RecorderTag, AudioRecorder>
+    {
+        public RecorderAdornmentTagger(IWpfTextView view, ITagAggregator<RecorderTag> dataTagger) : base(view, dataTagger)
+        {
+        }
+
+        public static ITagger<IntraTextAdornmentTag> GetTagger(IWpfTextView view, Lazy<ITagAggregator<RecorderTag>> recorderTagger)
+        {
+            return view.Properties.GetOrCreateSingletonProperty(
+                () => new RecorderAdornmentTagger(view, recorderTagger.Value));
+        }
+
+        protected override AudioRecorder CreateAdornment(RecorderTag data, SnapshotSpan span)
+        {
+            return new AudioRecorder();
+        }
+
+        protected override bool UpdateAdornment(AudioRecorder adornment, RecorderTag data)
+        {
+            return true;
+        }
+
+        public override void Dispose()
+        {
+            view.Properties.RemoveProperty(typeof (RecorderAdornmentTagger));
+        }
+    }
+}
