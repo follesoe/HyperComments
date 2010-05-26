8715641ee4282029bae1cb149987598195536dd1
Jonas Folles <jonas@follesoe.no>
2010-05-26 20:05:26 +0200

    Added Recorder Adornment Tagger Provider.

---
+++ HyperComments/Recorder/RecorderAdornmentTaggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace HyperComments.Recorder
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("code")]
+    [TagType(typeof(IntraTextAdornmentTag))]
+    public class RecorderAdornmentTaggerProvider : IViewTaggerProvider
+    {
+        [Import]
+        internal IBufferTagAggregatorFactoryService BufferTagAggregatorFactoryService;
+
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            return RecorderAdornmentTagger.GetTagger(
+                (IWpfTextView)textView,
+                new Lazy<ITagAggregator<RecorderTag>>(
+                    () => BufferTagAggregatorFactoryService.CreateTagAggregator<RecorderTag>(textView.TextBuffer))) as ITagger<T>;
+            
+        }
+    }
+}
